c22d066079c25b84283edc241537e60f0fb57e2c
Ashraf Ali <ashrafalicitp@gmail.com>
2022-05-22 04:17:57 +0600

    style fabrics project setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        styleFabrics: {
+          primary: "#07F8C3",
+          secondary: "#FF9D00",
+          accent: "#4289FF",
+          neutral: "#282531",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
